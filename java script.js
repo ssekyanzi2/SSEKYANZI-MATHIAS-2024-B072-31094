006a06357531f53eba71b37cd3c30be17524a056
ssekyanzi2 <ssekyanzimathias8@gmail.com>
2024-12-05 15:11:59 +0300

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,9 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>MY FIRST PAGE</title></head>
+<script src="functions.js type="text/javaScript"></script><html>
+<body>
+<button onclick="My function();"> Click me</button></head>
+</body>
+</html>
